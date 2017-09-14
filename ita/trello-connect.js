d416df1616643b54d50ebc68d844037f3a0bbf54
giacomocrivellari <crivellari.giacomo@gmail.com>
2017-09-14 14:47:33 +0200

    auto compress format imgix

--- ita/trello-connect.js
+++ ita/trello-connect.js
@@ -26,14 +26,14 @@ jQuery.scrollDepth({
 
 
 
-
-
 /*! modernizr 3.3.1 (Custom Build) | MIT *
  * http://modernizr.com/download/?-video-setclasses !*/
 !function(e,n,a){function o(e){var n=p.className,a=Modernizr._config.classPrefix||"";if(f&&(n=n.baseVal),Modernizr._config.enableJSClass){var o=new RegExp("(^|\\s)"+a+"no-js(\\s|$)");n=n.replace(o,"$1"+a+"js$2")}Modernizr._config.enableClasses&&(n+=" "+a+e.join(" "+a),f?p.className.baseVal=n:p.className=n)}function s(e,n){return typeof e===n}function t(){var e,n,a,o,t,c,r;for(var p in l)if(l.hasOwnProperty(p)){if(e=[],n=l[p],n.name&&(e.push(n.name.toLowerCase()),n.options&&n.options.aliases&&n.options.aliases.length))for(a=0;a<n.options.aliases.length;a++)e.push(n.options.aliases[a].toLowerCase());for(o=s(n.fn,"function")?n.fn():n.fn,t=0;t<e.length;t++)c=e[t],r=c.split("."),1===r.length?Modernizr[r[0]]=o:(!Modernizr[r[0]]||Modernizr[r[0]]instanceof Boolean||(Modernizr[r[0]]=new Boolean(Modernizr[r[0]])),Modernizr[r[0]][r[1]]=o),i.push((o?"":"no-")+r.join("-"))}}function c(){return"function"!=typeof n.createElement?n.createElement(arguments[0]):f?n.createElementNS.call(n,"http://www.w3.org/2000/svg",arguments[0]):n.createElement.apply(n,arguments)}var i=[],l=[],r={_version:"3.3.1",_config:{classPrefix:"",enableClasses:!0,enableJSClass:!0,usePrefixes:!0},_q:[],on:function(e,n){var a=this;setTimeout(function(){n(a[e])},0)},addTest:function(e,n,a){l.push({name:e,fn:n,options:a})},addAsyncTest:function(e){l.push({name:null,fn:e})}},Modernizr=function(){};Modernizr.prototype=r,Modernizr=new Modernizr;var p=n.documentElement,f="svg"===p.nodeName.toLowerCase();Modernizr.addTest("video",function(){var e=c("video"),n=!1;try{(n=!!e.canPlayType)&&(n=new Boolean(n),n.ogg=e.canPlayType('video/ogg; codecs="theora"').replace(/^no$/,""),n.h264=e.canPlayType('video/mp4; codecs="avc1.42E01E"').replace(/^no$/,""),n.webm=e.canPlayType('video/webm; codecs="vp8, vorbis"').replace(/^no$/,""),n.vp9=e.canPlayType('video/webm; codecs="vp9"').replace(/^no$/,""),n.hls=e.canPlayType('application/x-mpegURL; codecs="avc1.42E01E"').replace(/^no$/,""))}catch(a){}return n}),t(),o(i),delete r.addTest,delete r.addAsyncTest;for(var d=0;d<Modernizr._q.length;d++)Modernizr._q[d]();e.Modernizr=Modernizr}(window,document);
 
 
+
 $(window).on("ready", function() {
+
 
 
   // highlight dell'attivo nella navbar
@@ -159,7 +159,7 @@ $(window).on("ready", function() {
       if (card.desc.indexOf("player.vimeo.com") > 0 || card.desc.indexOf("www.youtube.com") > 0) {
         var $clone = $model.clone();
         $clone.css("display","flex");
-        
+
         var $embed = $('<div class="mt-1 embed-responsive embed-responsive-16by9"></div>');
         var $iframe = $('<iframe ></iframe>');
         $iframe.attr("src", card.desc);
@@ -181,6 +181,7 @@ $(window).on("ready", function() {
 
       var url = card.attachments[0].url.replace("https://trello-attachments.s3.amazonaws.com", "http://galimberti.imgix.net");
       url = url + "?w=" + resolution;
+      url = url + "&auto=compress,format";
       url = url + "&mark=" + markurl;
       url = url + "&markscale=" + markscale + "&markpad=" + markpad;
       $clone.find("img").attr("src", url);
@@ -214,6 +215,7 @@ $(window).on("ready", function() {
         var $secondCol = $col.clone();
         var url = card.attachments[1].url.replace("https://trello-attachments.s3.amazonaws.com", "http://galimberti.imgix.net");
         url = url + "?w=" + resolution;
+        url = url + "&auto=compress,format";
         url = url + "&mark=" + markurl;
         url = url + "&markscale=" + markscale + "&markpad=" + markpad;
         $secondCol.find("img").attr("src", url);
@@ -332,7 +334,7 @@ $(window).on("ready", function() {
     } catch(e) {
     }
 
-   
+
     // $(document).find(".trello").hide();
 
   });
