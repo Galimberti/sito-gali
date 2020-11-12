810aef57c643699feb65d68bfce3ae5cd3235e49
Giorgio Seregni <giorgio.seregni@pingendo.com>
2020-11-12 10:08:05 +0100

    more

---
+++ .eleventy.js
@@ -0,0 +1,42 @@
+const pluginPingendo = require("pingendo-11ty");
+
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.setEjsOptions({
+    openDelimiter: "{{",
+    closeDelimiter: "}}"
+  });
+  eleventyConfig.addPlugin(pluginPingendo);
+  eleventyConfig.setDataDeepMerge(true);
+  eleventyConfig.setDynamicPermalinks(true);
+  eleventyConfig.setTemplateFormats([
+    "ejs",
+    "md",
+    "js",
+    "html",
+    "yml",
+    "jpg",
+    "css" // css is not yet a recognized template extension in Eleventy
+  ]);
+
+
+  return {
+    templateFormats: [
+      "ejs",
+      "md",
+      "njk",
+      "html",
+      "liquid",
+    ],
+    passthroughFileCopy: true,
+    markdownTemplateEngine: "ejs",
+    htmlTemplateEngine: "ejs",
+    dataTemplateEngine: "ejs",
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "build"
+    }
+  };
+
+};
